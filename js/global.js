0b87422d894c184cc00550426176069c6f4f1769
Bo <bophatin@gmail.com>
2019-05-09 23:08:06 +0200

    Ajout fichier global pour instancier des objets

---
+++ js/global.js
@@ -0,0 +1,16 @@
+
+$(function() {
+
+	// SLIDER
+	var slider1 = Object.create(Slider);
+	slider1.init("#slider1");
+
+ 	// MAP
+ 	var map1 = Object.create(Map);
+    map1.init("#map1");
+
+    // BOOKING
+    var booking1 = Object.create(Booking);
+    booking1.init("#booking1");
+    
+});
